7b189a9f666ea1fd8dcbd372d04e807b8db7ac20
Nevan Wichers <nevan.wichers@gmail.com>
2025-11-18 12:28:31 -0800

    Comment changes

--- runpod/runpod_setup.sh
+++ runpod/runpod_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# 1) Setup linux dependencies
+# Setup linux dependencies
 su -c 'apt-get update && apt-get install -y sudo'
 sudo apt-get install -y less nano htop ncdu nvtop lsof rsync btop jq
 
-# 2) Setup virtual environment
+# Setup virtual environment
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 uv python install 3.11
 
-# 3) Setup dotfiles and ZSH
+# Setup dotfiles and ZSH
 mkdir git
 cd git
 git clone https://github.com/wichersn/dotfiles.git
@@ -18,12 +18,12 @@ cd dotfiles
 chsh -s /usr/bin/zsh
 ./deploy.sh
 
-# 4) Setup github
+# Setup github
 chmod +x ./runpod/setup_github.sh
 ./runpod/setup_github.sh "nevan.wichers@gmail.com" "Nevan Wichers"
 cd ..
 
-# 5) Copy SSH authorized_keys from workspace (if present)
+# Copy SSH authorized_keys from workspace (if present)
 if [ -f "/workspace/.ssh/authorized_keys" ]; then
 mkdir -p "$HOME/.ssh"
 cp -f "/workspace/.ssh/authorized_keys" "$HOME/.ssh/authorized_keys"
@@ -31,11 +31,12 @@ else
 echo "Warning: /workspace/.ssh/authorized_keys not found; skipping copy."
 fi
 
+# Setup claude code.
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 nvm install --lts
 nvm use --lts
